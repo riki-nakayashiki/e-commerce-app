8edc728a5416cd7722eac72a0dc881d97d4dd3b7
riki-nakayashiki <riki.nakayashiki@gmail.com>
2023-08-29 16:43:31 -0700

    init

---
+++ client_app/src/App.js
@@ -0,0 +1,11 @@
+import TextReact from "./components/TextReact.jsx";
+
+function App() {
+    return (
+        <section>
+            <TextReact />
+        </section>
+    );
+}
+
+export default App;
