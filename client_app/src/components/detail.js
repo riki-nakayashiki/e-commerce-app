947207eb082f0263f9e75f975b232bb7e0474a51
Jeongeun Lee <jungeun9005@gmail.com>
2023-08-29 18:53:42 -0700

    clone

---
+++ client_app/src/components/detail.js
@@ -0,0 +1,22 @@
+import '../style/detail.css';
+
+function Detail(){
+    
+    return(
+        <div className='detail'>
+            <div className='product-img'>
+                <img src="http://assets.myntassets.com/v1/images/style/properties/7b661983ddd630db69f2847a53e25704_images.jpg"></img>
+            </div>
+            <div className='product-detail'>
+                <small>Topwear</small>
+                <h2>(name)Reebok Women Boat-Neck Wildberry Sweatshirt</h2>
+                <p>(type)Sweatshirts</p>
+                <p>(price)$344</p>
+                <p>(color)Pink</p>
+
+                <button>Add to Cart</button>
+            </div>           
+        </div>
+    )
+}
+export default Detail;
