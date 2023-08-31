acdfd1f80cced106fb1c03e3b3f32f2657a4bf33
DYagmur <30656517+DYagmur@users.noreply.github.com>
2023-08-31 14:47:13 +0300

    slider.css added

--- client_app/src/components/detail.jsx
+++ client_app/src/components/detail.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { useState, useEffect } from "react";
 import {useParams} from 'react-router-dom';
 import Header from "./Header.jsx";
-import Footer from "./FooterComponent.jsx";
+import Footer from "./Footer.jsx";
 
 export default function Detail(){
     const [jsonObj, setData] = useState([]);
